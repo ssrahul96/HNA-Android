0f09af1ce0fb9bf71e0938f90c422985404b7ea9
ssrahul96 <ssrahul96@gmail.com>
2018-02-13 22:14:19 +0530

    updated path

--- deploy.sh
+++ deploy.sh
@@ -18,10 +18,10 @@ cd app/build/outputs/apk/
 ls
 
 echo "debug directory"
-cd app/build/outputs/apk/debug
+cd app/build/outputs/apk/debug/
 ls
 
 echo "release directory"
-cd app/build/outputs/apk/release
+cd app/build/outputs/apk/release/
 ls
 
